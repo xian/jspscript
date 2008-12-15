10466855e1bdb17b20e541851f2a053ab3f356f0
Christian Williams <antixian666@gmail.com>
2008-12-15 02:06:24 +0000

    Pull JspScript.Env and JspScript.Template into their own class files.

--- build-demo.sh
+++ build-demo.sh
@@ -6,6 +6,8 @@ baseDir=..
 
 java -jar $baseDir/lib/js.jar -strict -w \
 	-f $baseDir/src/JspScript.js \
+	-f $baseDir/src/Env.js \
+	-f $baseDir/src/Template.js \
 	-f $baseDir/src/CoreTaglib.js \
 	-f $baseDir/src/Generator.js \
 	-f $baseDir/src/Parser.js \
